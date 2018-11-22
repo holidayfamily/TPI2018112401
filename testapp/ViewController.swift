76c910ebc325efc23c7cae0e76a1d7cb343635b7
yunyoung <yunyoung.ju@gmail.com>
2018-11-22 22:50:24 +0900

    test

--- testapp/ViewController.swift
+++ testapp/ViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var rest: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       //view.backgroundColor=UIColor.red
+        changeBackgroundColor()
+        
+        
     }
 
-
+    @IBAction func clickYellowButton(_ sender: Any) {
+        if(view.backgroundColor==UIColor.yellow){
+            viewtext.text = "잘했어"
+        }else{
+            viewtext.text = "다시해봐"
+        }
+        viewtext.text = "바탕화면 색에 해당하는 버튼을 눌러봐"
+        changeBackgroundColor()
+    }
+    
+    @IBAction func clickRadButton(_ sender: Any) {
+        if(view.backgroundColor==UIColor.red){
+            viewtext.text = "잘했어"
+        }else{
+            viewtext.text = "다시해봐"
+        }
+        viewtext.text = "바탕화면 색에 해당하는 버튼을 눌러봐"
+        changeBackgroundColor()
+    }
+    
+    
+    @IBAction func clickBlueButton(_ sender: Any) {
+        if(view.backgroundColor==UIColor.blue){
+            viewtext.text = "잘했어"
+        }else{
+            viewtext.text = "다시해봐"
+        }
+        viewtext.text = "바탕화면 색에 해당하는 버튼을 눌러봐"
+        changeBackgroundColor()
+    }
+    
+    @IBAction func clickWhiteButton(_ sender: Any) {
+        if(view.backgroundColor==UIColor.white){
+            viewtext.text = "잘했어"
+        }else{
+            viewtext.text = "다시해봐"
+        }
+        viewtext.text = "바탕화면 색에 해당하는 버튼을 눌러봐"
+        changeBackgroundColor()
+    }
+    
+    @IBOutlet weak var viewtext: UILabel!
+    
+    func changeBackgroundColor(){
+        let colornum = Int.random(in: 0 ... 3)
+        if(colornum==0){
+            view.backgroundColor=UIColor.red
+        } else if(colornum==1){
+            view.backgroundColor=UIColor.blue
+        } else if (colornum==2) {
+            view.backgroundColor=UIColor.white
+        }else {
+            view.backgroundColor=UIColor.yellow
+        }
+    
+    }
+    
 }
 
