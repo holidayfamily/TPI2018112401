de20494359c1cf6aa02e18b1c63bbd662b1efa2a
yunyoung <yunyoung.ju@gmail.com>
2018-11-24 15:44:14 +0900

    20181124  스위치문을 이용하여 button Action 을 구현함

--- testapp/ViewController.swift
+++ testapp/ViewController.swift
@@ -11,81 +11,76 @@ import CoreFoundation
 
 class ViewController: UIViewController {
     var startTime:CFAbsoluteTime = 0.0
+    
+    
+    @IBOutlet weak var textLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         changeBackgroundColor()
     }
-
-    @IBAction func clickYellowButton(_ sender: Any) {
-        print("clickYellowButton=\(view.backgroundColor==UIColor.yellow)")
-        if(view.backgroundColor==UIColor.yellow){
-            viewText.text = "잘했어"
-            checkTime()
+    
+    
+    @IBAction func clickButton(_ sender: UIButton) {
+        
+        let buttonid:String = sender.restorationIdentifier!
+        var checkflag:Bool = false
+        switch buttonid {
+        case "white":
+            if(view.backgroundColor == UIColor.white){
+                checkflag = true
+            }
+        case "blue":
+            if(view.backgroundColor == UIColor.blue){
+                checkflag = true
+            }
+        case "red":
+            if(view.backgroundColor == UIColor.red){
+                checkflag = true
+            }
+        case "yellow":
+            if(view.backgroundColor == UIColor.yellow){
+                checkflag = true
+            }
+        default:
+            checkflag = false
+        }
+        if(checkflag){
+            textLable.text = "잘했어 다시한번 눌러봐"
         }else{
-            viewText.text = "다시해봐"
+            textLable.text = "다시 눌러보자"
         }
-        
-        
-        
-    }
-    
-    @IBAction func clickRadButton(_ sender: Any) {
-        print("clickRadButton=\(view.backgroundColor==UIColor.red)")
-        if(view.backgroundColor==UIColor.red){
-            viewText.text = "잘했어"
-            checkTime()
-        }else{
-            viewText.text = "다시해봐"
-        }
-        
-    }
-    
-    
-    @IBAction func clickBlueButton(_ sender: Any) {
-        print("clickBlueButton=\(view.backgroundColor==UIColor.blue)")
-        if(view.backgroundColor==UIColor.blue){
-            viewText.text = "잘했어"
-            checkTime()
-        }else{
-            viewText.text = "다시해봐"
-        }
-        
-    }
-    
-    @IBAction func clickWhiteButton(_ sender: Any) {
-        print("clickWhiteButton=\(view.backgroundColor==UIColor.white)")
-        if(view.backgroundColor==UIColor.white){
-            viewText.text = "잘했어"
-            checkTime()
-        }else{
-            viewText.text = "다시해봐"
-        }
-        
-        
-    }
-    
-    
-    @IBOutlet weak var viewText: UITextField!
-    
-    func changeBackgroundColor(){
-     //   viewText.text = "바탕화면 색에 해당하는 버튼을 눌러봐"
-        let colornum = Int.random(in: 0 ... 3)
-        if(colornum==0){
-            view.backgroundColor=UIColor.red
-        } else if(colornum==1){
-            view.backgroundColor=UIColor.blue
-        } else if (colornum==2) {
-            view.backgroundColor=UIColor.white
-        }else {
-            view.backgroundColor=UIColor.yellow
-        }
-    
-    }
-    
-    func checkTime(){    
         changeBackgroundColor()
     }
     
+    
+
+    
+    func changeBackgroundColor(){
+        
+     //   textLable.text = "바탕화면 색에 해당하는 버튼을 눌러봐"
+        let colornum = Int.random(in: 0 ... 3)
+        if(colornum == 0){
+            view.backgroundColor = UIColor.red
+        } else if(colornum == 1){
+            view.backgroundColor = UIColor.blue
+        } else if (colornum == 2) {
+            view.backgroundColor = UIColor.white
+        }else {
+            view.backgroundColor = UIColor.yellow
+        }
+    }
+    
+    func checkTime(){
+        startTime = CFAbsoluteTimeGetCurrent()
+        var flag = true
+        while(flag){
+            let timeGab = CFAbsoluteTimeGetCurrent() - startTime
+            if(timeGab>2){
+                flag=false
+            }
+        }
+        changeBackgroundColor()
+    }
     
 }
 
