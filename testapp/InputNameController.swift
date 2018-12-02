796ef52ca07d13ba07e625e1c616ce6cca3b35ff
yunyoung <yunyoung.ju@gmail.com>
2018-12-02 22:52:56 +0900

    이름이 default propertiy에 없을 경우 입력을 받는 화면으로 이동하도록 수정

--- testapp/InputNameController.swift
+++ testapp/InputNameController.swift
@@ -9,15 +9,18 @@
 import UIKit
 
 class InputNameController: UIViewController{
-    @IBOutlet weak var InputName: UILabel!
-    @IBOutlet weak var inputNameBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let contentlavel = UILabel()
+        let inputNameBox = UITextField()
         self.view.backgroundColor = UIColor.white
-        print("in InputNameController")
-        InputName
-        InputName.text = "이름을 입력해주세요"
+        contentlavel.frame = CGRect(x:119,y:123, width:137,height:21)
+        inputNameBox.frame = CGRect(x:134,y:203, width:106,height:31)
+        inputNameBox.placeholder = "이름"
+        contentlavel.text = "이름을 입력해주세요"
+        self.view.addSubview(contentlavel)
+        self.view.addSubview(inputNameBox)
     }
     
 }
